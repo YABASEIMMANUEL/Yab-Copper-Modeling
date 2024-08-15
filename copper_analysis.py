9318ba2ea433989f7a4cece904a87f8173db0033
YABASEIMMANUEL <yabaseimmanuel@gmail.com>
2024-08-15 20:04:12 +0530

    copper_analysis.py

--- copper_analysis.py
+++ copper_analysis.py
@@ -33,7 +33,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_animation = load_lottieurl("https://raw.githubusercontent.com/YABASEIMMANUEL/copper_modeling/main/business-analysis.json")
+lottie_animation = load_lottieurl("https://raw.githubusercontent.com/YABASEIMMANUEL/Yab-Copper-Modeling/main/business-analysis.json")
 
 # Layout: left for menu and animation, right for content
 left, right = st.columns([1, 3], gap="medium")
